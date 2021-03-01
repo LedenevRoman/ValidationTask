34833a78b54d58c1819448f28bf4a0fad667ccf7
LedenevRoman <pommanbko@mail.ru>
2021-03-02 00:03:33 +0300

    fixed2

--- src/com/ValidationTask/ValidationSystemFactory.java
+++ src/com/ValidationTask/ValidationSystemFactory.java
@@ -1,5 +1,22 @@
 package com.ValidationTask;
 
-public interface ValidationSystemFactory {
-    ValidationSystem createValidationSystem();
+public class ValidationSystemFactory {
+
+    public Validator createValidationSystemFactoryByType(Object o) {
+        if (o instanceof String) {
+            return new StringValidator();
+        }
+        if (o instanceof Integer) {
+            return new IntegerValidator();
+        }
+        if (o instanceof Boolean) {
+            return new BooleanValidator();
+        }
+        if (o instanceof Character) {
+            return new CharacterValidator();
+        } else {
+            throw new ValidationFailedException();
+        }
+    }
+
 }

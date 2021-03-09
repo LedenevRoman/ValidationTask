e616188688d8efa2e28b3a3b09ca9817d03e8979
LedenevRoman <pommanbko@mail.ru>
2021-03-09 11:11:43 +0300

    add HashMap, add Test

--- src/com/ValidationTask/ValidationSystemFactory.java
+++ src/com/ValidationTask/ValidationSystemFactory.java
@@ -1,19 +1,21 @@
 package com.ValidationTask;
 
+import java.util.HashMap;
+
 public class ValidationSystemFactory {
+    private final static HashMap<Class, Validator> CLASS_VALIDATOR_HASH_MAP = new HashMap<>();
+
+    {
+        CLASS_VALIDATOR_HASH_MAP.put(String.class, new StringValidator());
+        CLASS_VALIDATOR_HASH_MAP.put(Integer.class, new IntegerValidator());
+        CLASS_VALIDATOR_HASH_MAP.put(Boolean.class, new BooleanValidator());
+        CLASS_VALIDATOR_HASH_MAP.put(Character.class, new CharacterValidator());
+    }
 
     public Validator createValidatorByType(Object o) {
-        if (o instanceof String) {
-            return new StringValidator();
-        }
-        if (o instanceof Integer) {
-            return new IntegerValidator();
-        }
-        if (o instanceof Boolean) {
-            return new BooleanValidator();
-        }
-        if (o instanceof Character) {
-            return new CharacterValidator();
+
+        if (o instanceof String || o instanceof Integer || o instanceof Boolean || o instanceof Character) {
+            return CLASS_VALIDATOR_HASH_MAP.get(o.getClass());
         } else {
             throw new ValidationFailedException();
         }
---
+++ test/com/ValidationTask/ValidationSystemFactoryTest.java
@@ -0,0 +1,108 @@
+package com.ValidationTask;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ValidationSystemFactoryTest {
+    private ValidationSystemFactory validationSystemFactory;
+    private static final String STRING_WITH_UPPERCASE_FISRT_LETTER = "Aloha";
+    private static final String STRING_WITH_LOWERCASE_FIRST_LETTER = "alOha";
+    private static final int ZERO = 0;
+    private static final int FIVE = 5;
+    private static final int ELEVEN = 11;
+    private static final boolean TRUE = true;
+    private static final boolean FALSE = false;
+    private static final char UPPERCASE_CHAR = 'A';
+    private static final char LOWERCASE_CHAR = 'a';
+    private static final char NOT_LETTER = '?';
+    private static final double DOUBLE_TYPE_NUMBER = 3.14;
+
+    @Before
+    public void setUp() {
+        validationSystemFactory = new ValidationSystemFactory();
+    }
+
+    @After
+    public void tearDown() {
+        validationSystemFactory = null;
+    }
+
+    @Test
+    public void validateStringWithUpperCaseFirstLetter() {
+        boolean actual = validationSystemFactory.createValidatorByType(STRING_WITH_UPPERCASE_FISRT_LETTER)
+                .validate(STRING_WITH_UPPERCASE_FISRT_LETTER);
+        Assert.assertTrue(actual);
+    }
+
+    @Test
+    public void validateStringWithLowerCaseFirstLetter() {
+        boolean actual = validationSystemFactory.createValidatorByType(STRING_WITH_LOWERCASE_FIRST_LETTER)
+                .validate(STRING_WITH_LOWERCASE_FIRST_LETTER);
+        Assert.assertFalse(actual);
+    }
+
+    @Test
+    public void validateIntZero() {
+        boolean actual = validationSystemFactory.createValidatorByType(ZERO)
+                .validate(ZERO);
+        Assert.assertFalse(actual);
+    }
+
+    @Test
+    public void validateIntFive() {
+        boolean actual = validationSystemFactory.createValidatorByType(FIVE)
+                .validate(FIVE);
+        Assert.assertTrue(actual);
+    }
+
+    @Test
+    public void validateIntEleven() {
+        boolean actual = validationSystemFactory.createValidatorByType(ELEVEN)
+                .validate(ELEVEN);
+        Assert.assertFalse(actual);
+    }
+
+    @Test
+    public void validateBooleanTrue() {
+        boolean actual = validationSystemFactory.createValidatorByType(TRUE)
+                .validate(TRUE);
+        Assert.assertTrue(actual);
+    }
+
+    @Test
+    public void validateBooleanFalse() {
+        boolean actual = validationSystemFactory.createValidatorByType(FALSE)
+                .validate(FALSE);
+        Assert.assertFalse(actual);
+    }
+
+    @Test
+    public void validateCharUpperCase() {
+        boolean actual = validationSystemFactory.createValidatorByType(UPPERCASE_CHAR)
+                .validate(UPPERCASE_CHAR);
+        Assert.assertTrue(actual);
+    }
+
+    @Test
+    public void validateCharLowerCase() {
+        boolean actual = validationSystemFactory.createValidatorByType(LOWERCASE_CHAR)
+                .validate(LOWERCASE_CHAR);
+        Assert.assertFalse(actual);
+    }
+
+    @Test
+    public void validateCharNotLetter() {
+        boolean actual = validationSystemFactory.createValidatorByType(NOT_LETTER)
+                .validate(NOT_LETTER);
+        Assert.assertFalse(actual);
+    }
+
+    @Test (expected = ValidationFailedException.class)
+    public void validateDoubleTypeNumber() {
+        validationSystemFactory.createValidatorByType(DOUBLE_TYPE_NUMBER)
+                .validate(DOUBLE_TYPE_NUMBER);
+    }
+
+}

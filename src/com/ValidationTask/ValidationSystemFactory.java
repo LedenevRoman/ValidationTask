d529e965f6bd3be95c3ec31b4946134c58ce74c7
LedenevRoman <pommanbko@mail.ru>
2021-03-10 11:37:23 +0300

    update

--- src/com/ValidationTask/ValidationSystemFactory.java
+++ src/com/ValidationTask/ValidationSystemFactory.java
@@ -1,9 +1,10 @@
 package com.ValidationTask;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class ValidationSystemFactory {
-    private static final HashMap<Class, Validator> CLASS_VALIDATOR_HASH_MAP = new HashMap<>();
+    private static final Map<Class, Validator> CLASS_VALIDATOR_HASH_MAP = new HashMap<>();
 
     static {
         CLASS_VALIDATOR_HASH_MAP.put(String.class, new StringValidator());
@@ -14,7 +15,7 @@ public class ValidationSystemFactory {
 
     public Validator createValidatorByType(Object o) {
 
-        if (o instanceof String || o instanceof Integer || o instanceof Boolean || o instanceof Character) {
+        if (CLASS_VALIDATOR_HASH_MAP.containsKey(o.getClass())) {
             return CLASS_VALIDATOR_HASH_MAP.get(o.getClass());
         } else {
             throw new ValidationFailedException();
